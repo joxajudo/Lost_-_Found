3548cc2e14b1b1d0b24f96fa35bfdc7b28a530f0
murodullayevabduvali972@gmail.com <abduvalimurodullayev972@gmail.com>
2023-12-04 17:22:22 +0500

    permission

--- app/views/other.py
+++ app/views/other.py
@@ -1,6 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.parsers import MultiPartParser
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from app.models import Category, Item
+from app.permission import IsAuthorOrReadOnly
 from app.serializers.other import CategorySerializer, ItemSerializer
 
 
@@ -12,3 +15,5 @@ class CategoryViewSet(viewsets.ModelViewSet):
 class ItemViewSet(viewsets.ModelViewSet):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly, ]
+    parser_classes = [MultiPartParser, ]
