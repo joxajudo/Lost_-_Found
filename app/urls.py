4dcccef0d2493bf61f1d16446bc8d083e45147db
joxajudo <jalilovjahongir99@gmail.com>
2023-12-06 11:43:03 +0500

    User

--- app/urls.py
+++ app/urls.py
@@ -1,21 +1,15 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import path
 
 from app.views.change_password import ChangePasswordView
 from app.views.forgot_password import ForgotPasswordView, VerifyForgotPhoneNumberView
 from app.views.login_view import LoginAPIView
 from app.views.logout_view import LogoutAPIView
 from app.views.message import MessageListCreateAPIView, MessageRetrieveAPIView
-from app.views.other import CategoryViewSet, ItemViewSet
+from app.views.other import ItemViewSet, ItemListCreateAPIView, CategoryViewSet
 from app.views.register_view import RegisterView
 from app.views.verification_code import VerifyPhoneNumberView
 
-router = DefaultRouter()
-router.register(r'categories', CategoryViewSet)
-router.register(r'items', ItemViewSet)
-
 urlpatterns = [
-    path('', include(router.urls)),
     path("register/", RegisterView.as_view(), name='register'),
     path("register-verify/", VerifyPhoneNumberView.as_view(), name='register-verify'),
     path("login/", LoginAPIView.as_view(), name='login'),
@@ -25,4 +19,8 @@ urlpatterns = [
     path('logout/', LogoutAPIView.as_view(), name='logout'),
     path('api/messages/', MessageListCreateAPIView.as_view(), name='message-list-create'),
     path('api/message/<int:pk>/', MessageRetrieveAPIView.as_view(), name='message-retrieve'),
+
+    path('item/', ItemListCreateAPIView.as_view(), name='item'),
+    path('item<int:pk>/', ItemViewSet.as_view(), name='item'),
+    path('category/', CategoryViewSet.as_view(), name='category'),
 ]
--- app/views/other.py
+++ app/views/other.py
@@ -1,24 +1,35 @@
 from rest_framework import viewsets
 from rest_framework.filters import SearchFilter
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.parsers import MultiPartParser
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly, IsAdminUser
 
 from app.models import Category, Item
 from app.permission import IsAuthorOrReadOnly
 from app.serializers.other import CategorySerializer, ItemSerializer
 
 
-class CategoryViewSet(viewsets.ModelViewSet):
+class CategoryViewSet(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class ItemViewSet(viewsets.ModelViewSet):
+class ItemListCreateAPIView(ListCreateAPIView):
     queryset = Item.objects.all()
     serializer_class = ItemSerializer
     permission_classes = [IsAuthorOrReadOnly, ]
     parser_classes = [MultiPartParser, ]
     filter_backends = [SearchFilter, ]
     search_fields = ['name', 'description']
+
+
+class ItemViewSet(RetrieveUpdateDestroyAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    permission_classes = [IsAuthorOrReadOnly, ]
+    parser_classes = [MultiPartParser, ]
+    filter_backends = [SearchFilter, ]
+    search_fields = ['name', 'description']
+
 
 
